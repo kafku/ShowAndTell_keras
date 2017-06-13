91329ba9a0bb87610095bab9b73ef8bd81a712cb
kafku <kazunoko93@gmail.com>
2017-06-13 17:40:26 +0900

    set envs

--- train_on_raiden.sh
+++ train_on_raiden.sh
@@ -4,9 +4,18 @@
 #$ -N train_ShowAndTell
 #$ -e raiden_error.log
 #$ -o raiden_print.log
-#$ -adds l_hard h_vmem 50G
-#$ -adds l_hard m_mem_free 50G
 #$ -jc nvcr-cuda8_g8.168h
+# -adds l_hard h_vmem 50G
+# -adds l_hard m_mem_free 80G
+
+
+export MY_PROXY_URL="http://10.1.10.1:8080/"
+export HTTP_PROXY=$MY_PROXY_URL
+export HTTPS_PROXY=$MY_PROXY_URL
+export FTP_PROXY=$MY_PROXY_URL
+export http_proxy=$MY_PROXY_URL
+export https_proxy=$MY_PROXY_URL
+export ftp_proxy=$MY_PROXY_URL
 
 source ~/.bashrc
 python train.py
