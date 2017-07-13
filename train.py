bc369576d7f3c96763e6542d4e577be2d796b831
kafku <kazunoko93@gmail.com>
2017-07-13 15:30:11 +0900

    resnet -> inceptionv3

--- train.py
+++ train.py
@@ -5,11 +5,12 @@ import os
 from functools import partial
 import numpy as np
 import tensorflow as tf
-from keras.optimizers import RMSprop, SGD
+from keras.optimizers import RMSprop, SGD, Adam
 from keras.callbacks import ReduceLROnPlateau, CSVLogger, EarlyStopping, ModelCheckpoint
 from keras.models import load_model
-from keras.applications import ResNet50
-from keras.applications.resnet50 import preprocess_input
+#from keras.applications import ResNet50
+#from keras.applications.resnet50 import preprocess_input
+from keras.applications.inception_v3 import InceptionV3, preprocess_input
 from maeshori.nlp_utils import create_word_dict
 from maeshori.caps_utils import CocoGenerator
 from maeshori.gen_utils import stack_batch
@@ -31,16 +32,17 @@ print("max_sentence_length: %s"%max_sentence_length)
 # configuration
 num_gpu = 2
 img_channels = 3
-img_rows = 224
-img_cols = 224
+img_rows = 299
+img_cols = 299
 num_classes = 1000
 
 # create resnet model instance
 print("Loading image model")
-img_model = ResNet50(weights='imagenet',
-                     input_shape=(img_rows, img_cols, img_channels),
-                     include_top=False, # without softmax layer (set True for training)
-                     classes=num_classes)
+img_model = InceptionV3(weights='imagenet',
+                        input_shape=(img_rows, img_cols, img_channels),
+                        include_top=False, # without softmax layer (set True for training)
+                        pooling='avg',
+                        classes=num_classes)
 img_model.trainable = False
 
 img_feature_dim = img_model.output_shape[-1]
@@ -65,7 +67,7 @@ coco_train = CocoGenerator('./COCO/', 'train2014',
                            on_memory=True,
                            feature_extractor=deep_cnn_feature,
                            img_size=(img_rows, img_cols),
-                           cache='./COCO/cache/resnet50_train.pkl')
+                           cache='./COCO/cache/inceptionv3_train.pkl')
 # validation data
 coco_val = CocoGenerator('./COCO/', 'val2014',
                          word_dict=coco_train.word_dict,
@@ -74,7 +76,7 @@ coco_val = CocoGenerator('./COCO/', 'val2014',
                          on_memory=True,
                          feature_extractor=deep_cnn_feature,
                          img_size=(img_rows, img_cols),
-                         cache='./COCO/cache/resnet50_val.pkl')
+                         cache='./COCO/cache/inceptionv3_val.pkl')
 
 
 # load model
@@ -84,18 +86,18 @@ if num_gpu == 1:
                                max_sentence_length=max_sentence_length)
 else:
     with tf.device('/cpu:0'):
-        #im2txt_model = ShowAndTell(coco_train.vocab_size, img_feature_dim=img_feature_dim,
-        #                           max_sentence_length=max_sentence_length)
-        im2txt_model = load_model('./results/model_weight/weights_rmsprop64_28-2.93_.hdf5', compile=False)
+        im2txt_model = ShowAndTell(coco_train.vocab_size, img_feature_dim=img_feature_dim,
+                                   max_sentence_length=max_sentence_length)
+        #im2txt_model = load_model('./results/model_weight/weights_rmsprop64_28-2.93_.hdf5', compile=False)
     im2txt_model = make_parallel(im2txt_model, num_gpu)
-im2txt_model.compile(loss="categorical_crossentropy", optimizer=RMSprop(), metrics=['acc'])
+im2txt_model.compile(loss="categorical_crossentropy", optimizer=Adam(), metrics=['acc'])
 
 # callbacks
-lr_reducer = ReduceLROnPlateau(factor=0.5, cooldown=0, patience=5, min_lr=0.5e-6)
+lr_reducer = ReduceLROnPlateau(factor=0.5, cooldown=5, patience=5, min_lr=0.5e-6)
 early_stopper = EarlyStopping(min_delta=0.001, patience=100)
-checkpoint = ModelCheckpoint(filepath="./results/model_weight/weights_rmsprop128_2_{epoch:02d}-{val_loss:.2f}_.hdf5",
+checkpoint = ModelCheckpoint(filepath="./results/model_weight/weights_rmsprop64_inceptionv3_{epoch:02d}-{val_loss:.2f}_.hdf5",
                              save_best_only=True)
-csv_logger = CSVLogger('./results/logs/show_and_tell_rmsprop128.csv')
+csv_logger = CSVLogger('./results/logs/show_and_tell_rmsprop64_inceptionv3.csv')
 ifttt_url = 'https://maker.ifttt.com/trigger/keras_callback/with/key/' + os.environ['IFTTT_SECRET']
 ifttt_notify = IftttMakerWebHook(ifttt_url, job_name='caption_lstm')
 
@@ -108,7 +110,7 @@ coco_val_gen = coco_val.generator(img_size=(img_rows, img_cols),
                                   feature_extractor=deep_cnn_feature,
                                   maxlen=max_sentence_length, padding='post')
 
-factor = 64
+factor = 32
 if num_gpu > 1:
     coco_train_gen = stack_batch(coco_train_gen, num_gpu * factor)
     coco_val_gen = stack_batch(coco_val_gen, num_gpu * factor)
